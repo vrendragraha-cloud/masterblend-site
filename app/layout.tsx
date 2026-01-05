f13cb8e6cbbf8043331865faa117eb981776e912
vrendragraha-cloud <vrendragraha@gmail.com>
2026-01-05 07:21:04 +0000

    analytics Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import Script from "next/script";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 import { LanguageProvider } from "@/components/LanguageProvider";
 
